361aa4cccab8a4d2a622d150874462ede70f0f24
Tomi <82027176+tomi090@users.noreply.github.com>
2021-11-23 12:23:57 +0200

    Add project files.

---
+++ KauppaListaApi/Controllers/KauppaListaController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using KauppaListaApi.Models;
+using System.Linq;
+using System;
+
+namespace KauppaListaApi.Controllers
+{
+    
+}
---
+++ KauppaListaApi/Models/Tuotteet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace KauppaListaApi.Models
+{
+    public partial class Tuotteet
+    {
+        public int Tuotteet1 { get; set; }
+        public string Nimi { get; set; }
+        public int Määrä { get; set; }
+        public int Id { get; set; }
+    }
+}
